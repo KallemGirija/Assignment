f57bf2cb051cbc09813b3791175da2ee9cd52aa6
KallemGirija <123890806+KallemGirija@users.noreply.github.com>
2023-02-23 13:19:23 +0530

    Add files via upload

---
+++ app/bill.ts
@@ -0,0 +1,34 @@
+export class Bill
+{
+     billId:number;
+     restaurantName:String;
+     itemCount:number;
+	 grossBillAmount:number;
+	 gstAmount:number;
+
+     
+     
+
+    constructor(
+        
+        billId:number,
+        restaurantName:String,
+        itemCount:number,
+        grossBillAmount:number,
+        gstAmount:number,
+        
+       
+
+        
+    ){
+       
+        this.billId=billId;
+        this.restaurantName=restaurantName;
+        this.itemCount=itemCount;
+        this.grossBillAmount=grossBillAmount;
+        this.gstAmount=gstAmount;
+       
+
+    }
+
+}
